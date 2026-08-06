2672972d313943205942dc8f4714b4a5c7bdf2a3
Mufeeth007 <mufazalmufeeth007@gmail.com>
2026-08-06 13:52:44 +0530

    doctor new edit

--- doctor/lib/screens/home_screen.dart
+++ doctor/lib/screens/home_screen.dart
@@ -25,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
 
     final controller =
-        Provider.of<LoginController>(context, listen: false);
+        Provider.of<LoginController>(context);
 
     final currentDentist =
         widget.dentist ?? controller.currentDentist;
